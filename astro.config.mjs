fde0ffb041a14b91e3cadfc44c2813f0ad703b0b
BAJJDY <bajjdy@users.noreply.github.com>
2026-02-11 09:08:13 +0800

    fix-all-errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-		site: "https://your-domain.com",
+		site: "https://blog.bajjdy.cc.cd",
 		base: "/",
 		trailingSlash: "ignore",
 	devToolbar: {
